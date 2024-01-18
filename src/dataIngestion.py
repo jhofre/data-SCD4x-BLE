3778b9d9857882181e559f5abb9df8f98685e262
jhofydu <jhofreeduardo@hotmail.com>
2024-01-18 15:12:20 +0100

    SeC:/Users/User/anaconda3/envs/ac3env/python.exe c:/Users/User/anaconda3/envs/ac3env/ac3Project/src/dataIngestion.py

--- src/dataIngestion.py
+++ src/dataIngestion.py
@@ -16,38 +16,45 @@ service_uuid_data_transfer = "00008004-B38D-4985-720E-0F993A68EE41"
 raw_sensor_frame = [] 
         
 
+async def get_co2():
+    co2_msb = raw_sensor_frame[0][7]
+    co2_lsb = raw_sensor_frame[0][6]
+    #print(raw_sensor_frame)
+    #print(co2_msb, co2_lsb)
+    co2_hex = f"{co2_msb:02x}{co2_lsb:02x}"
+    co2_dec = int(co2_hex, 16)
+    return co2_dec
+    
+
+async def get_temperature():
+    temperature_msb = raw_sensor_frame[0][3]
+    temperature_lsb = raw_sensor_frame[0][2]
+    """ print(raw_sensor_frame)
+    print(temperature_msb, temperature_lsb) """
+    temperature_hex = f"{temperature_msb:02x}{temperature_lsb:02x}"
+    temperature_dec_ticks = int(temperature_hex, 16)
+    temperature = -45 + ((175.0 * temperature_dec_ticks ) / ((2 **16) - 1))
+    return temperature
+    
+    
+async def get_humidity():
+    humidity_msb = raw_sensor_frame[0][5]
+    humidity_lsb = raw_sensor_frame[0][4]
+    """ print(raw_sensor_frame)
+    print(humidity_msb, humidity_lsb) """
+    humidity_hex = f"{humidity_msb:02x}{humidity_lsb:02x}"
+    humidity_dec_ticks = int(humidity_hex, 16)
+    humidity = ((100.0 * humidity_dec_ticks) / ((2 **16) - 1))
+    return humidity
+   
+
 #Contains two frames: 1) The header  2)Sampling data
 # keep only the sampling data in the variable
-def notification_handler(characteristic: BleakGATTCharacteristic, data: bytearray):
+async def notification_handler(characteristic: BleakGATTCharacteristic, data: bytearray):
     global raw_sensor_frame
     raw_sensor_frame = []
     raw_sensor_frame.append(list(data))
-
-def get_co2():
-    print("CO2 List:", raw_sensor_frame)
-    co2_msb = raw_sensor_frame[0][7]
-    co2_lsb = raw_sensor_frame[0][6]
-    co2_hex = f"{co2_msb:02x}{co2_lsb:02x}"
-    co2_dec = int(co2_hex, 16)
-    print(f"- Co2 concentration: {co2_dec} ppm\n")
-
-def get_temperature():
-    co2_msb = raw_sensor_frame[0][3]
-    co2_lsb = raw_sensor_frame[0][2]
-    #print(co2_msb, co2_lsb)
-    co2_hex = f"{co2_msb:02x}{co2_lsb:02x}"
-    co2_dec_ticks = int(co2_hex, 16)
-    temperature = -45 + ((175.0 * co2_dec_ticks ) / ((2 **16) - 1))
-    print(f"- Temperature: {temperature} °C\n")
-    
-def get_humidity():
-    co2_msb = raw_sensor_frame[0][5]
-    co2_lsb = raw_sensor_frame[0][4]
-    #print(co2_msb, co2_lsb)
-    co2_hex = f"{co2_msb:02x}{co2_lsb:02x}"
-    humidity_dec_ticks = int(co2_hex, 16)
-    humidity = ((100.0 * humidity_dec_ticks) / ((2 **16) - 1))
-    print(f"- Humidity: {humidity} %\n")
+    await asyncio.sleep(20)
 
 """ Connecting to the Sensor """
 async def interface_sensor():
@@ -55,17 +62,18 @@ async def interface_sensor():
         print('\nEstablishing connection')
         print("Sensor connected!\n")
         """ Sensor Notification contaning the data frame """
-        for rep in range(5):
+        for rep in range(40):
             await scd41_three.start_notify(service_uuid_data_transfer, notification_handler)
             await asyncio.sleep(3.0)
             print("******DATA INTAKING SENSIRION SCD41 CO₂ Sensor Demonstrator******\n", "------Notification ",rep,"-------", "\n" )
-            get_co2()
-            get_temperature()
-            get_humidity()
+            co2_concentration= await get_co2()
+            print(f"- Co2 concentration: {co2_concentration} ppm\n")
+            temperature = await get_temperature()
+            print(f"- Temperature: {temperature} °C\n")
+            humidity_percentage = await get_humidity()
+            print(f"- Humidity: {humidity_percentage} %\n")
+    print(raw_sensor_frame)       
 
-
-
-        
 
 
 "Main Co-routine"
